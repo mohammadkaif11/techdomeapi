757e4a38721693d61d135d634cf4aeacf1e801d2
mohammadkaif <mohammadkaif0211@gmail.com>
2022-04-20 23:00:43 +0530

    Adding user Model

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api1.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; } 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Roles     { get; set; }
+        public string Password  { get; set; }
+        public string IsActive { get; set; }    
+    }
+}
