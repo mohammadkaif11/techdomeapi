437ba33f7e4d5eb36e5b7628337cad78062fb824
mohammadkaif <mohammadkaif0211@gmail.com>
2022-04-21 00:24:17 +0530

    adding auth controller

---
+++ Controllers/Auth.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Auth : ControllerBase
+    {
+        
+    }
+}
